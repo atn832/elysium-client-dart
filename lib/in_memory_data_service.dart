d9c378eea23265ec1c11e500131dd349ab9145ed
root <atn832@gmail.com>
2018-08-27 10:41:28 +0000

    parse initial getmessages

--- lib/in_memory_data_service.dart
+++ lib/in_memory_data_service.dart
@@ -102,21 +102,496 @@ class InMemoryDataService extends MockClient {
       ]
     }
   };
+  static const getMessagesResponse = {
+    "chanList": [
+      {
+        "ID": 1,
+        "name": "Elysium"
+      }
+    ],
+    "chanUpdates": [
+      {
+        "chanID": 1,
+        "events": [
+          {
+            "ID": 735455,
+            "eventType": {
+              "ID": 7,
+              "name": "Session Resume",
+              "type": "SessionResume"
+            },
+            "source": {
+              "connection": {
+                "ID": 304804,
+                "IP": {
+                  "ID": 2951,
+                  "IP": "202.89.121.20"
+                },
+                "hostname": {
+                  "ID": 2711,
+                  "hostname": "202.89.121.20"
+                }
+              },
+              "datetime": "2018-08-27T09:29:08",
+              "entity": {
+                "ID": 5,
+                "entityType": {
+                  "ID": 2,
+                  "name": "User",
+                  "type": "User"
+                },
+                "name": "frun"
+              },
+              "location": {
+                "ID": 272649,
+                "accuracy": 69,
+                "altitude": null,
+                "altitudeAccuracy": null,
+                "heading": null,
+                "latitude": 25.0576449,
+                "locationName": null,
+                "longitude": 121.6147452,
+                "speed": null
+              },
+              "timeZone": {
+                "ID": 10,
+                "timeZone": "Asia/Taipei"
+              },
+              "userAgent": {
+                "ID": 525,
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+              }
+            }
+          },
+          {
+            "ID": 735456,
+            "eventType": {
+              "ID": 7,
+              "name": "Session Resume",
+              "type": "SessionResume"
+            },
+            "source": {
+              "connection": {
+                "ID": 304805,
+                "IP": {
+                  "ID": 4324,
+                  "IP": "17.154.10.75"
+                },
+                "hostname": {
+                  "ID": 4146,
+                  "hostname": "17.154.10.75"
+                }
+              },
+              "datetime": "2018-08-27T09:32:32",
+              "entity": {
+                "ID": 4,
+                "entityType": {
+                  "ID": 2,
+                  "name": "User",
+                  "type": "User"
+                },
+                "name": "atn"
+              },
+              "location": null,
+              "timeZone": {
+                "ID": 21,
+                "timeZone": "America/Los_Angeles"
+              },
+              "userAgent": {
+                "ID": 525,
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+              }
+            }
+          },
+          {
+            "ID": 735457,
+            "eventType": {
+              "ID": 6,
+              "name": "Session Pause",
+              "type": "SessionPause"
+            },
+            "source": {
+              "connection": null,
+              "datetime": "2018-08-27T09:39:34",
+              "entity": {
+                "ID": 5,
+                "entityType": {
+                  "ID": 2,
+                  "name": "User",
+                  "type": "User"
+                },
+                "name": "frun"
+              },
+              "location": null,
+              "timeZone": null,
+              "userAgent": null
+            }
+          },
+          {
+            "ID": 735458,
+            "content": "bon",
+            "eventType": {
+              "ID": 3,
+              "name": "Message",
+              "type": "Message"
+            },
+            "source": {
+              "connection": {
+                "ID": 304806,
+                "IP": {
+                  "ID": 4324,
+                  "IP": "17.154.10.75"
+                },
+                "hostname": {
+                  "ID": 4146,
+                  "hostname": "17.154.10.75"
+                }
+              },
+              "datetime": "2018-08-27T09:58:24",
+              "entity": {
+                "ID": 4,
+                "entityType": {
+                  "ID": 2,
+                  "name": "User",
+                  "type": "User"
+                },
+                "name": "atn"
+              },
+              "location": null,
+              "timeZone": {
+                "ID": 21,
+                "timeZone": "America/Los_Angeles"
+              },
+              "userAgent": {
+                "ID": 525,
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+              }
+            }
+          },
+          {
+            "ID": 735459,
+            "content": "débat philosophique encore une fois",
+            "eventType": {
+              "ID": 3,
+              "name": "Message",
+              "type": "Message"
+            },
+            "source": {
+              "connection": {
+                "ID": 304807,
+                "IP": {
+                  "ID": 4324,
+                  "IP": "17.154.10.75"
+                },
+                "hostname": {
+                  "ID": 4146,
+                  "hostname": "17.154.10.75"
+                }
+              },
+              "datetime": "2018-08-27T09:58:27",
+              "entity": {
+                "ID": 4,
+                "entityType": {
+                  "ID": 2,
+                  "name": "User",
+                  "type": "User"
+                },
+                "name": "atn"
+              },
+              "location": {
+                "ID": 272650,
+                "accuracy": 30,
+                "altitude": null,
+                "altitudeAccuracy": null,
+                "heading": null,
+                "latitude": 37.792830699999996,
+                "locationName": null,
+                "longitude": -122.42401169999998,
+                "speed": null
+              },
+              "timeZone": {
+                "ID": 21,
+                "timeZone": "America/Los_Angeles"
+              },
+              "userAgent": {
+                "ID": 525,
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+              }
+            }
+          },
+          {
+            "ID": 735460,
+            "content": "est-ce que je mets le code de mon nouveau elysium client sur github ou bitbucket ?",
+            "eventType": {
+              "ID": 3,
+              "name": "Message",
+              "type": "Message"
+            },
+            "source": {
+              "connection": {
+                "ID": 304808,
+                "IP": {
+                  "ID": 4324,
+                  "IP": "17.154.10.75"
+                },
+                "hostname": {
+                  "ID": 4146,
+                  "hostname": "17.154.10.75"
+                }
+              },
+              "datetime": "2018-08-27T09:58:35",
+              "entity": {
+                "ID": 4,
+                "entityType": {
+                  "ID": 2,
+                  "name": "User",
+                  "type": "User"
+                },
+                "name": "atn"
+              },
+              "location": {
+                "ID": 272651,
+                "accuracy": 30,
+                "altitude": null,
+                "altitudeAccuracy": null,
+                "heading": null,
+                "latitude": 37.792830699999996,
+                "locationName": null,
+                "longitude": -122.42401169999998,
+                "speed": null
+              },
+              "timeZone": {
+                "ID": 21,
+                "timeZone": "America/Los_Angeles"
+              },
+              "userAgent": {
+                "ID": 525,
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+              }
+            }
+          },
+          {
+            "ID": 735461,
+            "content": "allez",
+            "eventType": {
+              "ID": 3,
+              "name": "Message",
+              "type": "Message"
+            },
+            "source": {
+              "connection": {
+                "ID": 304809,
+                "IP": {
+                  "ID": 4324,
+                  "IP": "17.154.10.75"
+                },
+                "hostname": {
+                  "ID": 4146,
+                  "hostname": "17.154.10.75"
+                }
+              },
+              "datetime": "2018-08-27T10:03:15",
+              "entity": {
+                "ID": 4,
+                "entityType": {
+                  "ID": 2,
+                  "name": "User",
+                  "type": "User"
+                },
+                "name": "atn"
+              },
+              "location": {
+                "ID": 272652,
+                "accuracy": 30,
+                "altitude": null,
+                "altitudeAccuracy": null,
+                "heading": null,
+                "latitude": 37.792830699999996,
+                "locationName": null,
+                "longitude": -122.42401169999998,
+                "speed": null
+              },
+              "timeZone": {
+                "ID": 21,
+                "timeZone": "America/Los_Angeles"
+              },
+              "userAgent": {
+                "ID": 525,
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+              }
+            }
+          },
+          {
+            "ID": 735462,
+            "content": "ça sera github encore une fois",
+            "eventType": {
+              "ID": 3,
+              "name": "Message",
+              "type": "Message"
+            },
+            "source": {
+              "connection": {
+                "ID": 304810,
+                "IP": {
+                  "ID": 4324,
+                  "IP": "17.154.10.75"
+                },
+                "hostname": {
+                  "ID": 4146,
+                  "hostname": "17.154.10.75"
+                }
+              },
+              "datetime": "2018-08-27T10:03:17",
+              "entity": {
+                "ID": 4,
+                "entityType": {
+                  "ID": 2,
+                  "name": "User",
+                  "type": "User"
+                },
+                "name": "atn"
+              },
+              "location": {
+                "ID": 272653,
+                "accuracy": 30,
+                "altitude": null,
+                "altitudeAccuracy": null,
+                "heading": null,
+                "latitude": 37.792830699999996,
+                "locationName": null,
+                "longitude": -122.42401169999998,
+                "speed": null
+              },
+              "timeZone": {
+                "ID": 21,
+                "timeZone": "America/Los_Angeles"
+              },
+              "userAgent": {
+                "ID": 525,
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+              }
+            }
+          },
+          {
+            "ID": 735465,
+            "eventType": {
+              "ID": 5,
+              "name": "Session Stop",
+              "type": "SessionStop"
+            },
+            "source": {
+              "connection": {
+                "ID": 304813,
+                "IP": {
+                  "ID": 4324,
+                  "IP": "17.154.10.75"
+                },
+                "hostname": {
+                  "ID": 4146,
+                  "hostname": "17.154.10.75"
+                }
+              },
+              "datetime": "2018-08-27T10:13:57",
+              "entity": {
+                "ID": 4,
+                "entityType": {
+                  "ID": 2,
+                  "name": "User",
+                  "type": "User"
+                },
+                "name": "atn"
+              },
+              "location": {
+                "ID": 272656,
+                "accuracy": 30,
+                "altitude": null,
+                "altitudeAccuracy": null,
+                "heading": null,
+                "latitude": 37.792830699999996,
+                "locationName": null,
+                "longitude": -122.42401169999998,
+                "speed": null
+              },
+              "timeZone": {
+                "ID": 21,
+                "timeZone": "America/Los_Angeles"
+              },
+              "userAgent": {
+                "ID": 525,
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+              }
+            }
+          },
+        ],
+        "userList": [
+          {
+            "ID": 4,
+            "latestSource": {
+              "datetime": "2018-08-27T10:03:24",
+              "location": {
+                "ID": 272655,
+                "accuracy": 30,
+                "altitude": null,
+                "altitudeAccuracy": null,
+                "heading": null,
+                "latitude": 37.792830699999996,
+                "locationName": null,
+                "longitude": -122.42401169999998,
+                "speed": null
+              },
+              "timeZone": {
+                "ID": 21,
+                "timeZone": "America/Los_Angeles"
+              }
+            },
+            "name": "atn",
+            "on": true
+          },
+          {
+            "ID": 5,
+            "latestSource": {
+              "datetime": "2018-08-27T01:21:51",
+              "location": {
+                "ID": 272607,
+                "accuracy": 108,
+                "altitude": null,
+                "altitudeAccuracy": null,
+                "heading": null,
+                "latitude": 25.0575661,
+                "locationName": null,
+                "longitude": 121.61500389999999,
+                "speed": null
+              },
+              "timeZone": {
+                "ID": 10,
+                "timeZone": "Asia/Taipei"
+              }
+            },
+            "name": "frun",
+            "on": false
+          }
+        ],
+        "userListUpdated": true
+      }
+    ],
+    "lastEventID": -1,
+    "numMessages": -1,
+    "token": "1041471832",
+    "userID": 4,
+    "validResponse": true
+  };
   static Future<Response> _handler(Request request) async {
     var data;
     switch (request.method) {
       case 'GET':
-        data = loginActionResponse;
-        print(request.url);
-        // final id = int.tryParse(request.url.pathSegments.last);
-        // if (id != null) {
-        //   data = _heroesDb
-        //       .firstWhere((hero) => hero.id == id); // throws if no match
-        // } else {
-        //   String prefix = request.url.queryParameters['name'] ?? '';
-        //   final regExp = RegExp(prefix, caseSensitive: false);
-        //   data = _heroesDb.where((hero) => hero.name.contains(regExp)).toList();
-        // }
+        switch (request.url.pathSegments.last) {
+          case 'login.action':
+            data = loginActionResponse;
+            break;
+          case 'getmessages.action':
+            data = getMessagesResponse;
+            break;
+        }
         break;
       default:
         throw 'Unimplemented HTTP method ${request.method}';
--- lib/src/todo_list/http_chat_service.dart
+++ lib/src/todo_list/http_chat_service.dart
@@ -12,11 +12,13 @@ import '../person.dart';
 @Injectable()
 class HttpChatService extends TodoListService {
   static const _heroesUrl = "login.action?channel.name=Elysium&channel.password=&user.name=atn";
+  static const _getmessagesUrl = "getmessages.action?token=155567289&userID=4&timeZone=America%2FLos_Angeles&log=true&lastEventID=-1&numMessages=-1";
   final Client _http;
   
   HttpChatService(this._http);
   
 	static final frun = Person("frun");
+	static final atn = Person("atn");
 	
   List<Message> messageList = <Message>[
 		Message(frun, "second service."),
@@ -27,6 +29,12 @@ class HttpChatService extends TodoListService {
       final response = await _http.get(_heroesUrl);
       final data = _extractData(response) as Map<String, dynamic>;
       messageList.add(Message(frun, "Channel is " + (data["channel"]["name"] as String)));
+      final response2 = await _http.get(_getmessagesUrl);
+      final data2 = _extractData(response2) as Map<String, dynamic>;
+      data2["chanUpdates"][0]["events"]
+        .where((e) => e["eventType"]["type"] == "Message")
+        .map((e) => e["content"])
+        .forEach((s) => messageList.add(Message(atn, s as String)));
       return messageList;
     } catch (e) {
       throw _handleError(e);
