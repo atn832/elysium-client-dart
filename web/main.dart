fa88103f310f645ad87fe172e7a16f5a1e688f14
root <atn832@gmail.com>
2018-08-27 08:55:55 +0000

    introduce BrowserClient as httpclient

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,20 @@
 import 'package:angular/angular.dart';
 import 'package:quickstart/app_component.template.dart' as ng;
 
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
+
+import 'main.template.dart' as self;
+
+@GenerateInjector([
+  ClassProvider(Client, useClass: BrowserClient),
+  // Using a real back end?
+  // Import 'package:http/browser_client.dart' and change the above to:
+  //   ClassProvider(Client, useClass: BrowserClient),
+])
+
+final InjectorFactory injector = self.injector$Injector;
+
 void main() {
-  runApp(ng.AppComponentNgFactory);
+  runApp(ng.AppComponentNgFactory, createInjector: injector);
 }
