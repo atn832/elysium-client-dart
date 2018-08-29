f12e11edb2895997211cde57abf64097ddfd306a
root <atn832@gmail.com>
2018-08-29 12:41:36 +0000

    added input field to sign in page

--- lib/src/sign_in_component/sign_in_component.dart
+++ lib/src/sign_in_component/sign_in_component.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 import 'package:angular_router/angular_router.dart';
 
 import '../route_paths.dart';
@@ -10,12 +11,21 @@ import '../route_paths.dart';
   styleUrls: ['sign_in_component.css'],
   templateUrl: 'sign_in_component.html',
   directives: [
+    materialInputDirectives,
     routerDirectives,
   ],
 )
 
 class SignInComponent {
-  SignInComponent();
+  final Router _router;
+
+  String username = '';
+  
+  SignInComponent(this._router);
+
+  void signin() {
+    _router.navigate(chatUrl(username));
+  }
 
   String chatUrl(String username) =>
     RoutePaths.chat.toUrl(parameters: {usernameParam: '$username'});
