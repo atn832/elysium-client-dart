af6d6f024d853acc50a9d95ed2ff6d2708b97027
root <atn832@gmail.com>
2018-08-29 13:06:27 +0000

    persist username in localStorage

--- lib/src/sign_in_component/sign_in_component.dart
+++ lib/src/sign_in_component/sign_in_component.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:html';
 
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
@@ -16,14 +17,23 @@ import '../route_paths.dart';
   ],
 )
 
-class SignInComponent {
+class SignInComponent implements OnInit {
   final Router _router;
 
+  Storage localStorage;
   String username = '';
   
-  SignInComponent(this._router);
+  SignInComponent(this._router) {
+    localStorage = window.localStorage;
+  }
+
+  @override
+  Future<Null> ngOnInit() async {
+    username = localStorage['username'];
+  }
 
   void signin() {
+    localStorage['username'] = username;
     _router.navigate(chatUrl(username));
   }
 
