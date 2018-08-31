66fdc93a4d53198b0da0e77c98369fa789d70feb
Anh Tuan <atn832@gmail.com>
2018-09-01 08:51:04 +0900

    cleanup

--- web/main.dart
+++ web/main.dart
@@ -5,7 +5,6 @@ import 'package:angular_router/angular_router.dart';
 import 'package:elysium_client/src/in_memory_data_service.dart';
 import 'package:elysium_client/app_component.template.dart' as ng;
 
-import 'package:http/browser_client.dart';
 import 'package:http/http.dart';
 
 import 'main.template.dart' as self;
@@ -13,8 +12,6 @@ import 'main.template.dart' as self;
 @GenerateInjector([
   routerProvidersHash, // You can use routerProviders in production
   ClassProvider(Client, useClass: InMemoryDataService),
-  // Using a real back end? Use BrowserClient.
-  // ClassProvider(Client, useClass: BrowserClient),
 ])
 
 final InjectorFactory injector = self.injector$Injector;
--- web_prod/main.dart
+++ web_prod/main.dart
@@ -2,7 +2,6 @@ import 'package:angular/angular.dart';
 
 import 'package:angular_router/angular_router.dart';
 
-import 'package:elysium_client/src/in_memory_data_service.dart';
 import 'package:elysium_client/app_component.template.dart' as ng;
 
 import 'package:http/browser_client.dart';
@@ -12,8 +11,6 @@ import 'main.template.dart' as self;
 
 @GenerateInjector([
   routerProvidersHash, // You can use routerProviders in production
-  // ClassProvider(Client, useClass: InMemoryDataService),
-  // Using a real back end? Use BrowserClient.
   ClassProvider(Client, useClass: BrowserClient),
 ])
 
