2469f5d933ae4f04dc24d7aacb0b38693e04e449
Anh Tuan <atn832@gmail.com>
2018-09-04 19:43:25 +0900

    fixed upload of tzdb files to server

--- deploy.sh
+++ deploy.sh
@@ -13,3 +13,6 @@ scp build/index.html root@$HOST:$DESTINATION
 scp build/main.dart.js root@$HOST:$DESTINATION
 scp build/styles.css root@$HOST:$DESTINATION
 scp build/favicon.png root@$HOST:$DESTINATION
+
+# Required only once.
+# scp build/packages/time_machine/data/tzdb/* root@$HOST:$DESTINATION/packages/time_machine/data/tzdb/*
