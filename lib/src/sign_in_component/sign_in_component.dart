7fe8d5f9fc1073aa458d93d50677362db7ab452a
Anh Tuan <atn832@gmail.com>
2018-09-11 22:43:58 +0900

    implemented sign in with google

--- lib/src/sign_in_component/sign_in_component.dart
+++ lib/src/sign_in_component/sign_in_component.dart
@@ -4,6 +4,7 @@ import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:angular_router/angular_router.dart';
+import 'package:firebase/firebase.dart' as fb;
 
 import '../route_paths.dart';
 
@@ -20,12 +21,14 @@ import '../route_paths.dart';
 
 class SignInComponent implements OnInit {
   final Router _router;
+  final fb.Auth auth;
 
   Storage localStorage;
   String username = '';
   
-  SignInComponent(this._router) {
+  SignInComponent(this._router) : auth = fb.auth() {
     localStorage = window.localStorage;
+    _setAuthListener();
   }
 
   @override
@@ -40,4 +43,32 @@ class SignInComponent implements OnInit {
 
   String chatUrl(String username) =>
     RoutePaths.chat.toUrl(parameters: {usernameParam: '$username'});
+
+  
+  // Logins with the Google auth provider.
+  loginWithGoogle() async {
+    var provider = new fb.GoogleAuthProvider();
+    try {
+      await auth.signInWithPopup(provider);
+    } catch (e) {
+      print("Error in sign in with google: $e");
+    }
+  }
+
+  // Sets the auth event listener.
+  _setAuthListener() {
+    // When the state of auth changes (user logs in/logs out).
+    auth.onAuthStateChanged.listen((user) {
+      if (user == null) return;
+
+      _showProfile(user);
+    });
+  }
+
+  _showProfile(fb.User user) {
+    if (user.photoURL != null) {
+      print(user.photoURL);
+    }
+    print(user.displayName + " / " + user.email);
+  }
 }
--- web/main.dart
+++ web/main.dart
@@ -1,13 +1,14 @@
 import 'package:angular/angular.dart';
 
 import 'package:angular_router/angular_router.dart';
+import 'package:firebase/firebase.dart' as fb;
+import 'package:http/http.dart';
 
 import 'package:elysium_client/src/in_memory_data_service.dart';
 import 'package:elysium_client/app_component.template.dart' as ng;
 
-import 'package:http/http.dart';
-
 import 'main.template.dart' as self;
+import 'firebase_info.dart';
 
 @GenerateInjector([
   routerProvidersHash, // You can use routerProviders in production
@@ -17,5 +18,13 @@ import 'main.template.dart' as self;
 final InjectorFactory injector = self.injector$Injector;
 
 void main() {
+  fb.initializeApp(
+    apiKey: apiKey,
+    authDomain: authDomain,
+    databaseURL: databaseURL,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId
+  );
   runApp(ng.AppComponentNgFactory, createInjector: injector);
 }
