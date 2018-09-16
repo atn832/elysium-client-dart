45f1133994a2cf4df0b7ef9cde5b12e4937c088b
Anh Tuan <atn832@gmail.com>
2018-09-16 10:02:01 +0900

    add test for BubbleService: 2 messages from different authors

--- test/bubble_service.dart
+++ test/bubble_service.dart
@@ -53,13 +53,37 @@ void main() {
     final durationBetweenTheMessages = Duration(minutes: 10);
     final secondMessageTime = DateTime.now().add(durationBetweenTheMessages);
     bubbleService.addMessage(Message(Person('atn'), 'i am here', secondMessageTime, location));
-    // It should have reused the same bubble.
+    // It should have created a second bubble.
     expect(bubbles.length, 2);
     expect(bubbles[0].messages.length, 1);
     expect(bubbles[0].time, firstMessageTime);
     expect(bubbles[1].messages.length, 1);
     expect(bubbles[1].messages[0], 'i am here');
     expect(bubbles[1].time, secondMessageTime);
+  });
+
+  test('two bubbles from different authors', () {
+    final List<Bubble> bubbles = bubbleService.bubbles;
+    const location = null;
+    final firstMessageTime = DateTime.now();
+    bubbleService.addMessage(Message(Person('atn'), 'hello', firstMessageTime, location));
+    expect(bubbles.length, 1);
+    expect(bubbles[0].messages.length, 1);
+
+    // Add a second message long after the first one.
+    final secondMessageTime = DateTime.now().add(Duration(minutes: 1));
+    bubbleService.addMessage(Message(Person('frun'), 'hello back', secondMessageTime, location));
+
+    // It should have created a second bubble.
+    expect(bubbles.length, 2);
+    expect(bubbles[0].messages.length, 1);
+    expect(bubbles[0].time, firstMessageTime);
+    expect(bubbles[0].author.name, 'atn');
+    
+    expect(bubbles[1].messages.length, 1);
+    expect(bubbles[1].messages[0], 'hello back');
+    expect(bubbles[1].time, secondMessageTime);
+    expect(bubbles[1].author.name, 'frun');
   });
 
 }
