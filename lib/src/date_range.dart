8ff651a389ba8b80715bdb1a7d4d65932804ac09
Anh Tuan <atn832@gmail.com>
2018-09-16 10:40:36 +0900

    implement DateRange.isBefore, isAfter

--- lib/src/date_range.dart
+++ lib/src/date_range.dart
@@ -5,4 +5,12 @@ class DateRange {
   DateRange(this.startTime, this.endTime) {
     assert(startTime.isBefore(endTime) || startTime == endTime);
   }
+
+  bool isBefore(DateTime time) {
+    return endTime.isBefore(time) || endTime == time;
+  }
+
+  bool isAfter(DateTime time) {
+    return startTime.isAfter(time) || startTime == time;
+  }
 }
--- test/date_range.dart
+++ test/date_range.dart
@@ -23,4 +23,32 @@ void main() {
     expect(() => DateRange(end, start), throws);
   });
 
+  test('isBefore', () {
+    final start = DateTime.now();
+    final end = start.add(Duration(minutes: 1));
+    final dateRange = DateRange(start, end);
+
+    // DateRange is before something that happens after the date range ends.
+    final wayAfter = end.add(Duration(minutes: 1));
+    expect(dateRange.isBefore(wayAfter), true);
+
+    // DateRange is not before something that happened before the date range starts.
+    final wayBefore = start.subtract(Duration(minutes: 1));
+    expect(dateRange.isBefore(wayBefore), false);
+  });
+
+  test('isAfter', () {
+    final start = DateTime.now();
+    final end = start.add(Duration(minutes: 1));
+    final dateRange = DateRange(start, end);
+
+    // DateRange is after something that happens before the date range starts.
+    final wayBefore = start.subtract(Duration(minutes: 1));
+    expect(dateRange.isAfter(wayBefore), true);
+
+    // DateRange is not after something that happened after the date range starts.
+    final wayAfter = end.add(Duration(minutes: 1));
+    expect(dateRange.isAfter(wayAfter), false);
+  });
+
 }
