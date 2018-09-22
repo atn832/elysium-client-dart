074f2534e5039a0fbfb02fae9c8e62b7798f388d
Anh Tuan <atn832@gmail.com>
2018-09-22 19:20:18 +0900

    added missing dependency for firebase version

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,7 @@ import 'package:angular_router/angular_router.dart';
 import 'package:http/http.dart';
 
 import 'package:elysium_client/src/chat_service.dart';
+import 'package:elysium_client/src/firebase_chat_service.dart';
 import 'package:elysium_client/src/hardcoded_chat_service.dart';
 import 'package:elysium_client/src/http_chat_service.dart';
 import 'package:elysium_client/src/in_memory_data_service.dart';
@@ -14,8 +15,9 @@ import 'main.template.dart' as self;
 @GenerateInjector([
   routerProvidersHash, // You can use routerProviders in production
   const ClassProvider(ReverseGeocodingService),
+  const ClassProvider(ChatService, useClass: FirebaseChatService),
   // const ClassProvider(ChatService, useClass: HardcodedChatService),
-  const ClassProvider(ChatService, useClass: HttpChatService),
+  // const ClassProvider(ChatService, useClass: HttpChatService),
   const ClassProvider(Client, useClass: InMemoryDataService),
 ])
 final InjectorFactory injector = self.injector$Injector;
--- web_firebase/main.dart
+++ web_firebase/main.dart
@@ -1,6 +1,8 @@
 import 'package:angular/angular.dart';
 
 import 'package:angular_router/angular_router.dart';
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
 
 import 'package:elysium_client/app_component.template.dart' as ng;
 import 'package:elysium_client/src/chat_service.dart';
@@ -13,6 +15,8 @@ import 'main.template.dart' as self;
   routerProvidersHash, // You can use routerProviders in production
   const ClassProvider(ReverseGeocodingService),
   const ClassProvider(ChatService, useClass: FirebaseChatService),
+  // Required by ReverseGeocodingService.
+  const ClassProvider(Client, useClass: BrowserClient),
 ])
 
 final InjectorFactory injector = self.injector$Injector;
