39d98df7bdfb09075677b41df7763ac5938c51e5
Anh Tuan <atn832@gmail.com>
2018-09-23 09:46:10 +0900

    implemented parsing of links in fragment service

--- lib/src/message_list/fragment_service.dart
+++ lib/src/message_list/fragment_service.dart
@@ -1,8 +1,33 @@
+import 'dart:core';
+
 import 'fragment.dart';
+import 'link_fragment.dart';
 import 'text_fragment.dart';
+
+final linkRegex = RegExp(r"(https?:\/\/[^ ,)]*)");
 
 class FragmentService {
   List<Fragment> parse(String text) {
-    return [TextFragment(text)];
+    final linkMatches = linkRegex.allMatches(text);
+    if (linkMatches.isEmpty) {
+      return [TextFragment(text)];
+    }
+    final List<Fragment> fragments = [];
+    int latestMatchEndIndex = 0;
+    linkMatches.forEach((linkMatch) {
+      // Add the text before the link.
+      if (latestMatchEndIndex != linkMatch.start) {
+        fragments.add(TextFragment(text.substring(latestMatchEndIndex, linkMatch.start)));
+      }
+      // Add the link.
+      fragments.add(LinkFragment(text.substring(linkMatch.start, linkMatch.end)));
+      // Update latest match index.
+      latestMatchEndIndex = linkMatch.end;
+    });
+    // Add the text after the last link.
+    if (latestMatchEndIndex != text.length) {
+      fragments.add(TextFragment(text.substring(latestMatchEndIndex, text.length)));
+    }
+    return fragments;
   }
 }
--- test/fragment_service.dart
+++ test/fragment_service.dart
@@ -4,6 +4,7 @@ import 'package:angular_test/angular_test.dart';
 import 'package:test/test.dart';
 
 import 'package:elysium_client/src/message_list/fragment_service.dart';
+import 'package:elysium_client/src/message_list/link_fragment.dart';
 import 'package:elysium_client/src/message_list/text_fragment.dart';
 
 void main() {
@@ -15,11 +16,47 @@ void main() {
 
   tearDown(disposeAnyRunningTest);
 
-  test('empty initial state', () {
+  test('parse simple text', () {
     final fragments = fragmentService.parse("hello");
     expect(fragments.length, 1);
     expect(fragments[0].type, "text");
     expect((fragments[0] as TextFragment).text, "hello");
+  });
+
+  test('parse text with link at the end', () {
+    final fragments = fragmentService.parse("check this out: http://coolurl");
+    expect(fragments.length, 2);
+    expect(fragments[0].type, "text");
+    expect(fragments[1].type, "link");
+    final text = fragments[0] as TextFragment;
+    expect(text.text, "check this out: ");
+    final link = fragments[1] as LinkFragment;
+    expect(link.url, "http://coolurl");
+  });
+
+  test('parse text with https link at the start', () {
+    final fragments = fragmentService.parse("https://coolurl => love it haha");
+    expect(fragments.length, 2);
+    expect(fragments[0].type, "link");
+    expect(fragments[1].type, "text");
+    final link = fragments[0] as LinkFragment;
+    expect(link.url, "https://coolurl");
+    final text = fragments[1] as TextFragment;
+    expect(text.text, " => love it haha");
+  });
+
+  test('parse text with 2 link', () {
+    final fragments = fragmentService.parse("https://coolurl and http://www.gmail.com");
+    expect(fragments.length, 3);
+    expect(fragments[0].type, "link");
+    expect(fragments[1].type, "text");
+    expect(fragments[2].type, "link");
+    final link = fragments[0] as LinkFragment;
+    expect(link.url, "https://coolurl");
+    final text = fragments[1] as TextFragment;
+    expect(text.text, " and ");
+    final link2 = fragments[2] as LinkFragment;
+    expect(link2.url, "http://www.gmail.com");
   });
 
 }
