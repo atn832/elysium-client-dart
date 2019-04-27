233db38abca2710662179ae7264f68afdbfc1fba
Anh Tuan <atn832@gmail.com>
2019-04-27 15:51:20 +0900

    implement badge for count of words

--- lib/src/badge_service.dart
+++ lib/src/badge_service.dart
@@ -2,39 +2,79 @@ import 'dart:async';
 import 'dart:core';
 
 import 'package:angular/core.dart';
+import 'package:firebase/firebase.dart';
 import 'package:http/http.dart';
 
+import 'package:firebase/firebase.dart' as fb;
+import 'package:firebase/firestore.dart' as fs;
+
+import 'firebase_cards_info.dart';
 import 'api_key.dart';
 import 'http_util.dart';
 
 @Injectable()
 class BadgeService {
+  App app;
   final Client _http;
 
-  String _cache;
+  Map<String, String> _cache = Map();
 
-  BadgeService(this._http);
+  BadgeService(this._http) {
+    app = fb.initializeApp(
+      name: 'Cards',
+      apiKey: apiKey,
+      authDomain: authDomain,
+      databaseURL: databaseURL,
+      projectId: projectId,
+      storageBucket: storageBucket,
+      messagingSenderId: messagingSenderId
+    );
+  }
 
   Future<String> getBadge(String name) async {
-    if (name != '元元') {
+    if (_cache.containsKey(name)) {
+      return _cache[name];
+    }
+    if (name == '元元') {
+      final _url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&' +
+        'forUsername=Fruncoyy&key=$ApiKey';
+      try {
+        final response = await _http.get(_url);
+        final data = extractData(response) as Map<String, dynamic>;
+        final subscriberCount = data["items"][0]["statistics"]["subscriberCount"];
+        _cache[name] = '$subscriberCount fans';
+        setResetTimeout(name);
+        return _cache[name];
+      } catch (e) {
+        print(e);
+      }
+    } else if (name == '퇀') {
+      _cache[name] = await getStats();
+      setResetTimeout(name);
+      return _cache[name];
+    } else {
       return Future.value('');
     }
-    if (_cache != null) {
-      return _cache;
-    }
-    final _url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&' +
-      'forUsername=Fruncoyy&key=$ApiKey';
-    try {
-      final response = await _http.get(_url);
-      final data = extractData(response) as Map<String, dynamic>;
-      final subscriberCount = data["items"][0]["statistics"]["subscriberCount"];
-      _cache = '$subscriberCount fans';
-      Future.delayed(Duration(minutes: 30), () {
-        _cache = null;
-      });
-      return _cache;
-    } catch (e) {
-      print(e);
-    }
   }
+
+  setResetTimeout(String name) {
+    Future.delayed(Duration(minutes: 30), () {
+      _cache.remove(name);
+    });
+  }
+
+  Future<String> getStats() async {
+    fs.Firestore firestore = fb.firestore(app);
+    final latest = await firestore.collection("users").doc("SD9Do3G9ylbFMjVGeqdyaBO3LH02").collection("statistics")
+      .orderBy("datetime", "desc")
+      .limit(1)
+      .get();
+    final data = latest.docs.first.data();
+    final knownCards = data['LearningProgress.known'] +
+      data['LearningProgress.newish'] +
+      data['LearningProgress.wellKnown'];
+    final knownWords = knownCards / 2;
+    return '$knownWords words';
+  }
+  // /users/SD9Do3G9ylbFMjVGeqdyaBO3LH02/statistics/2019-04-26T17:44:07.957813
 }
